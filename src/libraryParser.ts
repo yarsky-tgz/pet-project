b8ca4205de9a29490ee639b34c2e53d00be3ba5e
southpalmire <hexus.southpalmire@gmail.com>
2021-11-01 21:46:41 +0300

    revert all

--- src/libraryParser.ts
+++ src/libraryParser.ts
@@ -1,57 +1,56 @@
-import EventEmitter from 'events';
-import fetch from 'node-fetch';
-import iconv from 'iconv-lite';
-import * as stream from 'stream';
+const EventEmitter = require('events')
+const fetch = require('node-fetch')
+const iconv = require('iconv-lite')
 
 class ReadlineInterface extends EventEmitter {
-  constructor(input: ReadlineInterface) {
-    super();
-    let brokenString: string;
-    input.on('data', (chunk: string) => {
-      if (chunk.indexOf('\n') !== -1) {
-        chunk.split('\n').forEach((element, index, array) => {
-          if (index === array.length - 1) {
-            brokenString = element;
-          } else if (index === 0) {
-            this.emit('line', brokenString + element);
-          } else {
-            this.emit('line', element);
-          }
-        });
-      } else {
-        brokenString += chunk;
-      }
-    })
+    constructor(input) {
+        super()
+        let brokenString
+        input.on('data', (chunk) => {
+            if (chunk.indexOf('\n') !== -1) {
+                chunk.split('\n').forEach((element, index, array) => {
+                    if (index === array.length -1) {
+                        brokenString = element
+                    }
+                    else if (index === 0) {
+                        this.emit('line', brokenString + element)
+                    }
+                    else {
+                        this.emit('line', element)
+                    }
+                })
+            }
+            else {
+                brokenString += chunk
+            }
+        })
 
-      .on('end', () => {
-        this.emit('line', brokenString);
-        this.emit('close');
-      });
-  }
+            .on('end', () => {
+                this.emit('line', brokenString)
+                this.emit('close')
+            })
+    }
 }
 
 const readline = {
-  createInterface({ input }:{[key:string]: ReadlineInterface}) {
-    return new ReadlineInterface(input);
-  },
+    createInterface({ input }) {
+        return new ReadlineInterface(input)
+    }
 };
 
 (async () => {
-  const data = await fetch('http://lib.ru/RUFANT/')
-    .then((response) => {
-      if (response && response.body) return response.body.pipe(iconv.decodeStream('koi8-r'));
-    });
-  const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/;
-  const autors: object[] = [];
-  const rl = readline.createInterface({ input: data as stream });
-  rl.on('line', (input: string) => {
-    type ExecRegex = RegExpExecArray & { groups: {} };
-    const result = regular.exec(input) as ExecRegex;
-    if (result) {
-      const { name, type, uri } = result.groups;
-      const newObject = { name, type, uri };
-      autors.push(newObject);
-    }
-  })
-    .on('close', () => console.log(autors));
-})();
+    const data = await fetch('http://lib.ru/RUFANT/')
+        .then(response => response.body.pipe(iconv.decodeStream('koi8-r')))
+    const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/
+    const autors = []
+    const rl = readline.createInterface({ input: data });
+    rl.on('line', (input) => {
+        const result = regular.exec(input)
+        if (result) {
+            const { name, type, uri } = result.groups
+            const newObject = { name, type, uri }
+            autors.push(newObject)
+        }
+    })
+        .on('close', () => console.log(autors))
+})()
